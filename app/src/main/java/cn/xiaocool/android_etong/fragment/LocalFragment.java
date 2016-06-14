a01d86de125b293ec2627514ec7c4cc911e0d053
潘宁 <1091390146@qq.com>
2016-06-15 00:01:26 +0800

    界面

--- app/src/main/java/cn/xiaocool/android_etong/fragment/LocalFragment.java
+++ app/src/main/java/cn/xiaocool/android_etong/fragment/LocalFragment.java
@@ -1,6 +1,5 @@
 package cn.xiaocool.android_etong.fragment;
 
-import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
@@ -9,20 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.xiaocool.android_etong.R;
 import cn.xiaocool.android_etong.adapter.lib.CycleViewPager;
-import cn.xiaocool.android_etong.adapter.lib.ViewFactory;
 import cn.xiaocool.android_etong.bean.ADInfo;
 
 /**
@@ -45,79 +36,79 @@ public class LocalFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_local,container,false);
         context = getActivity();
-        configImageLoader();
-        initialize();
+//        configImageLoader();
+//        initialize();
         return view;
     }
-    @SuppressLint("NewApi")
-    private void initialize() {
-
-        cycleViewPager = (CycleViewPager) getFragmentManager()
-                .findFragmentById(R.id.fragment_cycle_viewpager_content_local);
-
-        for(int i = 0; i < imageUrls.length; i ++){
-            ADInfo info = new ADInfo();
-            info.setUrl(imageUrls[i]);
-            info.setContent("图片-->" + i );
-            infos.add(info);
-        }
-
-        // 将最后一个ImageView添加进来
-        views.add(ViewFactory.getImageView(context, infos.get(infos.size() - 1).getUrl()));
-        for (int i = 0; i < infos.size(); i++) {
-            views.add(ViewFactory.getImageView(context, infos.get(i).getUrl()));
-        }
-        // 将第一个ImageView添加进来
-        views.add(ViewFactory.getImageView(context, infos.get(0).getUrl()));
-
-        // 设置循环，在调用setData方法前调用
-        cycleViewPager.setCycle(true);
-
-        // 在加载数据前设置是否循环
-        cycleViewPager.setData(views, infos, mAdCycleViewListener);
-        //设置轮播
-        cycleViewPager.setWheel(true);
-
-        // 设置轮播时间，默认5000ms
-        cycleViewPager.setTime(3000);
-        //设置圆点指示图标组居中显示，默认靠右
-        cycleViewPager.setIndicatorCenter();
-    }
-
-    private CycleViewPager.ImageCycleViewListener mAdCycleViewListener = new CycleViewPager.ImageCycleViewListener() {
-
-        @Override
-        public void onImageClick(ADInfo info, int position, View imageView) {
-            if (cycleViewPager.isCycle()) {
-                position = position - 1;
-                Toast.makeText(context,
-                        "position-->" + info.getContent(), Toast.LENGTH_SHORT)
-                        .show();
-            }
-
-        }
-
-    };
-
-    /**
-     * 配置ImageLoder
-     */
-    private void configImageLoader() {
-        // 初始化ImageLoader
-        @SuppressWarnings("deprecation")
-        DisplayImageOptions options = new DisplayImageOptions.Builder().showStubImage(R.drawable.icon_stub) // 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.drawable.icon_empty) // 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.drawable.icon_error) // 设置图片加载或解码过程中发生错误显示的图片
-                .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
-                .cacheOnDisc(true) // 设置下载的图片是否缓存在SD卡中
-                        // .displayer(new RoundedBitmapDisplayer(20)) // 设置成圆角图片
-                .build(); // 创建配置过得DisplayImageOption对象
-
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext()).defaultDisplayImageOptions(options)
-                .threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
-                .discCacheFileNameGenerator(new Md5FileNameGenerator()).tasksProcessingOrder(QueueProcessingType.LIFO).build();
-        ImageLoader.getInstance().init(config);
-    }
+//    @SuppressLint("NewApi")
+//    private void initialize() {
+//
+//        cycleViewPager = (CycleViewPager) getFragmentManager()
+//                .findFragmentById(R.id.fragment_cycle_viewpager_content_local);
+//
+//        for(int i = 0; i < imageUrls.length; i ++){
+//            ADInfo info = new ADInfo();
+//            info.setUrl(imageUrls[i]);
+//            info.setContent("图片-->" + i );
+//            infos.add(info);
+//        }
+//
+//        // 将最后一个ImageView添加进来
+//        views.add(ViewFactory.getImageView(context, infos.get(infos.size() - 1).getUrl()));
+//        for (int i = 0; i < infos.size(); i++) {
+//            views.add(ViewFactory.getImageView(context, infos.get(i).getUrl()));
+//        }
+//        // 将第一个ImageView添加进来
+//        views.add(ViewFactory.getImageView(context, infos.get(0).getUrl()));
+//
+//        // 设置循环，在调用setData方法前调用
+//        cycleViewPager.setCycle(true);
+//
+//        // 在加载数据前设置是否循环
+//        cycleViewPager.setData(views, infos, mAdCycleViewListener);
+//        //设置轮播
+//        cycleViewPager.setWheel(true);
+//
+//        // 设置轮播时间，默认5000ms
+//        cycleViewPager.setTime(3000);
+//        //设置圆点指示图标组居中显示，默认靠右
+//        cycleViewPager.setIndicatorCenter();
+//    }
+//
+//    private CycleViewPager.ImageCycleViewListener mAdCycleViewListener = new CycleViewPager.ImageCycleViewListener() {
+//
+//        @Override
+//        public void onImageClick(ADInfo info, int position, View imageView) {
+//            if (cycleViewPager.isCycle()) {
+//                position = position - 1;
+//                Toast.makeText(context,
+//                        "position-->" + info.getContent(), Toast.LENGTH_SHORT)
+//                        .show();
+//            }
+//
+//        }
+//
+//    };
+//
+//    /**
+//     * 配置ImageLoder
+//     */
+//    private void configImageLoader() {
+//        // 初始化ImageLoader
+//        @SuppressWarnings("deprecation")
+//        DisplayImageOptions options = new DisplayImageOptions.Builder().showStubImage(R.drawable.icon_stub) // 设置图片下载期间显示的图片
+//                .showImageForEmptyUri(R.drawable.icon_empty) // 设置图片Uri为空或是错误的时候显示的图片
+//                .showImageOnFail(R.drawable.icon_error) // 设置图片加载或解码过程中发生错误显示的图片
+//                .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
+//                .cacheOnDisc(true) // 设置下载的图片是否缓存在SD卡中
+//                        // .displayer(new RoundedBitmapDisplayer(20)) // 设置成圆角图片
+//                .build(); // 创建配置过得DisplayImageOption对象
+//
+//        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext()).defaultDisplayImageOptions(options)
+//                .threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
+//                .discCacheFileNameGenerator(new Md5FileNameGenerator()).tasksProcessingOrder(QueueProcessingType.LIFO).build();
+//        ImageLoader.getInstance().init(config);
+//    }
     @Override
     public void onClick(View v) {
 
--- app/src/main/java/cn/xiaocool/android_etong/fragment/PrefectureFragment.java
+++ app/src/main/java/cn/xiaocool/android_etong/fragment/PrefectureFragment.java
@@ -1,10 +1,12 @@
 package cn.xiaocool.android_etong.fragment;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import cn.xiaocool.android_etong.R;
 
@@ -12,13 +14,64 @@ import cn.xiaocool.android_etong.R;
  * Created by 潘 on 2016/6/12.
  */
 public class PrefectureFragment extends Fragment implements View.OnClickListener {
+    private Button[] mTabs;
+    private PrefectureMyFragment prefectureMyFragment;
+    private PrefectureProductFragment prefectureProductFragment;
+    private Fragment[] fragments;
+    private int index;
+    // 当前fragment的index
+    private int currentTabIndex;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_prefecture,container,false);
         return view;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        prefectureProductFragment = new  PrefectureProductFragment();
+        prefectureMyFragment = new PrefectureMyFragment();
+        fragments = new Fragment[]{prefectureMyFragment, prefectureProductFragment};
+        getChildFragmentManager().beginTransaction().add(R.id.fragment_container,prefectureMyFragment).commit();
+        initview();
+    }
+
+    private void initview() {
+        mTabs = new Button[2];
+        mTabs[0]  = (Button)getView().findViewById(R.id.btn_prefecture_my);
+        mTabs[0].setOnClickListener(this);
+        mTabs[1]  = (Button)getView().findViewById(R.id.btn_prefecture_product);
+        mTabs[1].setOnClickListener(this);
+        // 把第一个tab设为选中状态
+        mTabs[0].setSelected(true);
+    }
+
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_prefecture_my:
+                index = 0;
+                break;
+            case R.id.btn_prefecture_product:
+                index = 1;
+                break;
+        }
+        if (currentTabIndex != index) {
 
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+            transaction.hide(fragments[currentTabIndex]);
+            if (!fragments[index].isAdded()) {
+                transaction.add(R.id.fragment_container, fragments[index]);
+            }
+            transaction.show(fragments[index]);
+            transaction.commit();
+        }
+
+        mTabs[currentTabIndex].setSelected(false);
+        // 把当前tab设为选中状态
+        mTabs[index].setSelected(true);
+        currentTabIndex = index;
     }
 }
