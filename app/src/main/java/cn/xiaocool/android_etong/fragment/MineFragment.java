d48a6ac264d7f3bc0e9f0cb6b9da4848cac58ff9
潘宁 <1091390146@qq.com>
2016-07-04 22:10:40 +0800

    添加popupmenu

--- app/src/main/java/cn/xiaocool/android_etong/fragment/MineFragment.java
+++ app/src/main/java/cn/xiaocool/android_etong/fragment/MineFragment.java
@@ -9,10 +9,13 @@ import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,8 +26,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import cn.xiaocool.android_etong.R;
-import cn.xiaocool.android_etong.UI.Mine.BusinessActivity;
 import cn.xiaocool.android_etong.UI.LoginActivity;
+import cn.xiaocool.android_etong.UI.Mine.BusinessActivity;
 import cn.xiaocool.android_etong.UI.Mine.MineEditActivity;
 import cn.xiaocool.android_etong.dao.CommunalInterfaces;
 import cn.xiaocool.android_etong.net.constant.WebAddress;
@@ -37,9 +40,10 @@ import static cn.xiaocool.android_etong.util.StatusBarHeightUtils.getStatusBarHe
  * Created by 潘 on 2016/6/12.
  */
 public class MineFragment extends Fragment implements View.OnClickListener {
+    private ImageView img_setup;
     private CircleImageView img_mine_head;
     private RelativeLayout ry_line;
-    private Button btn_zhuxiao,btn_kaidian;
+    private Button btn_kaidian;
     private TextView tx_mine_name;
     private Context context;
     private Handler handler = new Handler(){
@@ -90,10 +94,11 @@ public class MineFragment extends Fragment implements View.OnClickListener {
         tx_mine_name = (TextView)getView().findViewById(R.id.tx_mine_name);
         img_mine_head = (CircleImageView)getView().findViewById(R.id.img_mine_head);
         img_mine_head.setOnClickListener(this);
-        btn_zhuxiao=(Button)getView().findViewById(R.id.btn_zhuxiao);
-        btn_zhuxiao.setOnClickListener(this);
         btn_kaidian=(Button)getView().findViewById(R.id.btn_kaidian);
         btn_kaidian.setOnClickListener(this);
+        img_setup=(ImageView)getView().findViewById(R.id.img_setup);
+        img_setup.setOnClickListener(this);
+
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -112,14 +117,44 @@ public class MineFragment extends Fragment implements View.OnClickListener {
             case R.id.img_mine_head:
                 startActivityForResult(new Intent(context, MineEditActivity.class), 1);
                 break;
-            case R.id.btn_zhuxiao:
-                startActivity(new Intent(context, LoginActivity.class));
-                getActivity().finish();
-                Toast.makeText(context,"退出登录",Toast.LENGTH_SHORT).show();
-                break;
             case R.id.btn_kaidian:
                 startActivity(new Intent(context, BusinessActivity.class));
                 break;
+            case R.id.img_setup:
+                showPopupMenu(img_setup);
+                break;
+
         }
+    }
+
+    private void showPopupMenu(View view) {
+        Log.e("popupmenu","success");
+        // View当前PopupMenu显示的相对View的位置
+        PopupMenu popupMenu = new PopupMenu(context,view);
+        //menu 布局
+        popupMenu.getMenuInflater().inflate(R.menu.main,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.quit:
+                        startActivity(new Intent(context, LoginActivity.class));
+                        getActivity().finish();
+                        Toast.makeText(context,"退出登录成功",Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
+//        // PopupMenu关闭事件 重写方法
+//        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+//            @Override
+//            public void onDismiss(PopupMenu menu) {
+//                Toast.makeText(context, "关闭PopupMenu", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        popupMenu.show();
+
     }
 }
